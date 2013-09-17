abb4d06f51d2eabbca181e6d0e588ef832a20311
Silas Sao <silassao@gmail.com>
2013-09-16 17:07:13 -0700

    add team photos; add animation for phones; get slider ready; unfinished

--- js/global.js
+++ js/global.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+  var phones = $('#phones');
+  function runIt() {
+    phones.animate({top:'+=10'}, 1000);
+    phones.animate({top:'-=10'}, 1000, runIt);
+  }
+  phones.fadeIn(2000);
+  runIt();
+})
