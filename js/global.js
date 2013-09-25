11809bcd51d7b43e64593517bd3c31decea73b21
Silas Sao <silassao@gmail.com>
2013-09-24 23:46:01 -0700

    finish slideshow styling and hover animation for team photos

--- js/global.js
+++ js/global.js
@@ -6,4 +6,23 @@ $(document).ready(function() {
   }
   phones.fadeIn(2000);
   runIt();
+
+  $('.team-members li').hover(
+    function() {
+      $(this).addClass('on');
+    }, function() {
+      $(this).removeClass('on');
+    }
+  );
+
+  /** custom slider navs **/
+  $('.slider-nav a#prev').click(function(e) {
+    e.preventDefault();
+    $('.slidesjs-previous').trigger('click');
+  });
+
+  $('.slider-nav a#next').click(function(e) {
+    e.preventDefault();
+    $('.slidesjs-next').trigger('click');
+  });
 })
