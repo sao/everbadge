aa6edc3d74d1d35e5f6659aa92bfdf692edba95b
Silas Sao <silassao@gmail.com>
2013-10-11 17:03:44 -0700

    mobilfy shit; retinise; mobile nav; responsive breakpoints css and js

--- js/global.js
+++ js/global.js
@@ -1,19 +1,48 @@
 $(document).ready(function() {
-  var phones = $('#phones');
-  function runIt() {
-    phones.animate({top:'+=10'}, 1000);
-    phones.animate({top:'-=10'}, 1000, runIt);
-  }
-  phones.fadeIn(2000);
-  runIt();
 
-  $('.team-members li').hover(
-    function() {
-      $(this).addClass('on');
-    }, function() {
-      $(this).removeClass('on');
-    }
+  $('.retina').retinise();
+
+  var jPM = $.jPanelMenu({
+    menu: '#mobile-menu',
+    trigger: '.mobile-menu-trigger',
+    openPosition: '80%'
+  });
+
+  var phones = $('#phones');
+  function animatePhone() {
+    phones.animate({top:'+=10'}, 1000);
+    phones.animate({top:'-=10'}, 1000, animatePhone);
+  }
+  // $(animatePhone);
+
+  $("#slides").slidesjs({
+    width: 947,
+    height: 701,
+  });
+
+  var jRes = jRespond([
+    {
+        label: 'small',
+        enter: 0,
+        exit: 1024
+    },{
+        label: 'large',
+        enter: 1024,
+        exit: 10000
+    }]
   );
+
+  jRes.addFunc({
+    breakpoint: 'small',
+    enter: function() {
+      jPM.on();
+      // phones.stop(true, false);
+    },
+    exit: function() {
+      jPM.off();
+      // animatePhone();
+    }
+  });
 
   /** custom slider navs **/
   $('.slider-nav a#prev').click(function(e) {
