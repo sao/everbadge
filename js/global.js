6cf4d44dac4c73d1033117f169351615abcbe84e
Silas Sao <silassao@gmail.com>
2013-10-15 18:18:07 -0700

    finish signup page, desktop and mobile

--- js/global.js
+++ js/global.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
+  jQuery.fn.center = function () {
+      this.css("position","absolute");
+      this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+                                                  $(window).scrollTop()) + "px");
+      this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+                                                  $(window).scrollLeft()) + "px");
+      return this;
+  }
+
+  // $('.authbox').center();
+  // $(window).resize(function() {
+  //   $('.authbox').center();
+  // })
+
   $('.retina').retinise();
 
   var jPM = $.jPanelMenu({
